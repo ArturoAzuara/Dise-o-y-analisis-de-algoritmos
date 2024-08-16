43d2cf24709f40567336147592f19bd64530e88e
ArturoAzuara <125943772+ArturoAzuara@users.noreply.github.com>
2024-08-15 21:54:09 -0600

    Tarea 2

--- Tarea_2/main.py
+++ Tarea_2/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+numeros = [1, 2, 3]  # Asignacion, T(n) = 1
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(f"Entrada: {numeros}")  # Asignacion, T(n) = 1
 
+combinaciones = 0  # Asignacion, T(n) = 1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for n_1 in numeros:  # Asignacion, T(n) = n
+    for n_2 in numeros:  # Asignacion, T(n) = n
+        print(n_1, n_2)  # Asignacion, T(n) = 1
+        combinaciones += 1  # Asignacion, T(n) = 1
 
+print(f"Final: {combinaciones}")  # Asignacion, T(n) = 1
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# T(n) = 1+1+1+n+n+1+1+1 = n^2 + 6
