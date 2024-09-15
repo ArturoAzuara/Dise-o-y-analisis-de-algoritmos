b045384e9bd8270b4666e8a09a9764fc39a9d220
ArturoAzuara <125943772+ArturoAzuara@users.noreply.github.com>
2024-09-14 20:39:38 -0600

    Tarea_5

--- Tarea_5/BusquedaBinaria.py
+++ Tarea_5/BusquedaBinaria.py
@@ -1,14 +1,15 @@
 def busqueda_binaria(data, num_buscar):
-    izquierda = 0
-    derecha = len(data) - 1
-    while izquierda <= derecha:
+    izquierda = 0               #T(n) = 1
+    derecha = len(data) - 1     #T(n) = 1
+    while izquierda <= derecha: #T(n) = log n
         div = (izquierda + derecha) // 2
         if data[div] == num_buscar:
-            return div
+            return div  #T(n) = 1
         elif data[div] < num_buscar:
-            izquierda = div + 1
+            izquierda = div + 1 #T(n) = 1
         else:
-            derecha = div - 1
-    return -1
+            derecha = div - 1 #T(n) = 1
+    return -1 #T(n) = 1
 
-
+#T(n) = 2 + log n + 4 
+#Complejidad = O(log n)
--- Tarea_5/busquedaLineal.py
+++ Tarea_5/busquedaLineal.py
@@ -1,8 +1,11 @@
 def busqueda_lineal(data, num_buscar):
-    for i in range(len(data)):
+    for i in range(len(data)):      #T(n) = n
         if num_buscar == data[i]:
-            return i
+            return i                #T(n) = 1
 
-    return -1
+    return -1                       #T(n) = 1
+
+#T(n) = n + 1 + 1 = n + 2
+#Complejidad = O(n)
 
 
