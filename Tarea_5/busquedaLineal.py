e0e2ff1a4969ded3444a196393969ec445a0e4fc
ArturoAzuara <125943772+ArturoAzuara@users.noreply.github.com>
2024-09-14 19:00:58 -0600

    actualizacion

--- Tarea_5/busquedaLineal.py
+++ Tarea_5/busquedaLineal.py
@@ -1,11 +1,11 @@
 def busqueda_lineal(data, num_buscar):
     for i in range(len(data)):
         if num_buscar == data[i]:
-            return print(f"El numero {num_buscar}, se encuentra en la posición {i}")
+            return i
 
-    return print(f"El numero {num_buscar}, no se encuentra en {data} ")
+    return -1
 
 
 numeros = [4, 5, 1, 8, 6, 9, 3]
 '''        0  1  2  3  4  5  6        '''
-busqueda_lineal(numeros, 200)
+print(busqueda_lineal(numeros, 3))
---
+++ Tarea_5/busquedaLinealCentinela.py
@@ -0,0 +1,9 @@
+def busqueda_centinela(data, num_buscar):
+    tmp = 0
+    for i in range(len(data)):
+
+
+
+datos = [3, 4, 7, 9, 2, 5]
+'''      0  1  2  3  4  5      '''
+busqueda_centinela(datos, 3)
