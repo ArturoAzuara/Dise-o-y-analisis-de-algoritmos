79455c4977968aaa98dc556b26f79c2c1a00d5ce
ArturoAzuara <125943772+ArturoAzuara@users.noreply.github.com>
2024-09-14 19:43:13 -0600

    update

--- Tarea_5/busquedaLinealCentinela.py
+++ Tarea_5/busquedaLinealCentinela.py
@@ -1,9 +1,15 @@
 def busqueda_centinela(data, num_buscar):
-    tmp = 0
-    for i in range(len(data)):
-
+    tmp = data[-1]
+    data[-1] = num_buscar
+    i = 0
+    while data[i] != num_buscar:
+        i += 1
+    data[-1] = tmp
+    if i < len(data) - 1 or data[-1] == num_buscar:
+        return i
+    return -1
 
 
 datos = [3, 4, 7, 9, 2, 5]
 '''      0  1  2  3  4  5      '''
-busqueda_centinela(datos, 3)
+print(busqueda_centinela(datos, 8))
