a849ae2a41eeb5c9e9a95fc065256c4f93718fb7
ArturoAzuara <125943772+ArturoAzuara@users.noreply.github.com>
2024-10-09 10:01:53 -0600

    Update Ejemplo_1.py
    changes

--- Tarea_7/Ejemplo_1.py
+++ Tarea_7/Ejemplo_1.py
@@ -1,9 +1,15 @@
 A = [-9, 3, 5, -2, 9, -7, 4, 8, 6]
 
-mayor = A[0] * A[1]
-for i in A:
-    for j in A:
-        mult = i * j
+mayor = A[0] * A[1]  
+num1 = A[0]
+num2 = A[1]
+
+for i in range(len(A)):
+    for j in range(i + 1, len(A)):  
+        mult = A[i] * A[j]
         if mult > mayor:
             mayor = mult
-            print(f"Los numeros cuyo producto es mas alto, son: {i} x{j}={mayor}")
+            num1 = A[i]
+            num2 = A[j]
+
+print(f"Los números cuyo producto es más alto, son: {num1} x {num2} = {mayor}")
